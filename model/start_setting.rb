b8da6d94918d543eb73ac6f6d85afd592c26a949
abab <ababup1192@gmail.com>
2013-06-02 21:59:46 +0900

    GUI製作中
    楽しくなってきたｗｗ

---
+++ model/start_setting.rb
@@ -0,0 +1,6 @@
+
+
+class StartSetting
+
+
+end
--- rating_main.rb
+++ rating_main.rb
@@ -4,38 +4,41 @@ require "tk"
 require "./model/execute_management.rb"
 require "./model/file_management.rb"
 require "./model/point_management.rb"
+require "./model/start_setting.rb"
+require "./view/start_dialog.rb"
 
 class RatingMain
   def initialize
+    MainWindow.new
   end
 end
 
+class MainWindow
+  def initialize
+    @start_setting = StartSetting.new
+    #ウィンドウのサイズ
+    root = TkRoot.new{
+      title "採点ツール"
+      geometry "950x480"
+    }
+    file_menu = TkMenu.new(root)
+
+    file_menu.add('command',
+      'label'     => "採点開始",
+      'command'   => proc{StartDialog.new},
+      'underline' => 0)
+    file_menu.add('command',
+      'label'     => "終了",
+      'command'   => proc{root.destroy},
+      'underline' => 0)
+    menu_bar = TkMenu.new
+    menu_bar.add('cascade','menu'  => file_menu,'label' => "File")
+
+    root.menu(menu_bar)
+    TkButton.new(root, 'text' => "採点開始", 'command' => proc{ StartDialog.new(self).view }).pack
+    Tk.mainloop
+  end
+  attr_accessor :start_setting
+end
+
 RatingMain.new
-
-#ウィンドウのサイズ
-root = TkRoot.new{
-  title "Rating Tool"
-  geometry "950x480"
-}
-file_menu = TkMenu.new(root)
-menu_click = Proc.new {
-  exit
-}
-file_menu.add('command',
-  'label'     => "採点開始",
-  'command'   => menu_click,
-  'underline' => 0)
-file_menu.add('command',
-  'label'     => "終了",
-  'command'   => menu_click,
-  'underline' => 0)
-menu_bar = TkMenu.new
-menu_bar.add('cascade',
- 'menu'  => file_menu,
- 'label' => "File")
-
-root.menu(menu_bar)
-
-Tk.mainloop
-
-
